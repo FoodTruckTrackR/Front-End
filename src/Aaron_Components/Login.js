8bef0f8d234d4e60a352a356d1bbabe728756e2a
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-20 18:58:45 -0700

    Added user state to hold the value for each input. Created a userChange function to fill the place of the onchange attribute. This will simply set the state of the user to whatever is inputed, and a userSubmit function that will clear the form and currently just console logs the information.

--- src/Aaron_Components/Login.js
+++ src/Aaron_Components/Login.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const LoginForm = () => {
+
+    const [user, setUser] = useState({
+        username: '',
+        password: ''
+    })
+
+    const userChange = event => {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const userSubmit = event => {
+        event.preventDefault();
+        axios.post('https://reqres.in/api/users', user)
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+        setUser({
+            username: '',
+            password: ''
+        })
+    }
+
     return (
-        <form>
+        <form onSubmit={userSubmit}>
           <label>
               Username: 
               <input 
                 type='text'
                 name='username'
                 id='username'
+                value={user.username}
+                onChange={userChange}
               />
         </label>
         <label>
@@ -17,6 +44,8 @@ const LoginForm = () => {
                 type='password'
                 name='password'
                 id='password'
+                value={user.password}
+                onChange={userChange}
             />    
         </label>
         <button type='submit'>Login</button>  
