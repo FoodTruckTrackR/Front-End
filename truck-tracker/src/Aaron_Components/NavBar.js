3a3f21915ad55b036c2c4931083299f425005ea4
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-22 18:08:53 -0700

    Added styling for the anchors in the NavBar

--- truck-tracker/src/Aaron_Components/NavBar.js
+++ truck-tracker/src/Aaron_Components/NavBar.js
@@ -4,15 +4,24 @@ import styled from 'styled-components';
 const NavStyle = styled.nav`
     background-color: #51240f;
     color: rgb(255, 213, 0, 0.8);
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-content: space-around;
+`
+
+const StyleAnch = styled.a`
+    margin-right: 2%;
 `
 
 const NavComp = () => {
     return(
         <NavStyle>
-            <a>Home</a>
-            <a>About</a>
-            <a>Profile</a>
-            <a>Sign out</a>
+            <StyleAnch>Home</StyleAnch>
+            <StyleAnch>About</StyleAnch>
+            <StyleAnch>Profile</StyleAnch>
+            <StyleAnch>Sign out</StyleAnch>
         </NavStyle>
     )
 }
