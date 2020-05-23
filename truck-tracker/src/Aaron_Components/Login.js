ed86053f7f9ccc8a0d8b291a3beedc506508d918
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-22 17:30:18 -0700

    Styled the login component with its' links

--- truck-tracker/src/Aaron_Components/Login.js
+++ truck-tracker/src/Aaron_Components/Login.js
@@ -2,13 +2,29 @@ import React from 'react';
 import LoginDiner from './Login_Diner';
 import LoginOperator from './Login_Operator'
 import { Link, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import '../App.css'
+
+const LoginDiv = styled.div`
+    margin-left: 25%;
+    display: flex;
+    flex-direction: column;
+`
+
+const LinkDiv = styled.div`
+    margin-left: 25%;
+`
 
 const LoginForm = () => {
     return(
         <div>
+            <LoginDiv>
             <h1>Are you a diner or a truck operator?</h1>
-            <Link to='/dinerlogin'>Diner</Link>
-            <Link to='/operatorlogin'>Truck operator</Link>
+            <LinkDiv>
+                <Link className='Links' to='/dinerlogin'>Diner</Link>
+                <Link className='Links' to='/operatorlogin'>Truck operator</Link>
+            </LinkDiv>
+        </LoginDiv>
             <Route path='/dinerlogin' component={LoginDiner} />
             <Route path='/operatorlogin' component={LoginOperator} />
         </div>
