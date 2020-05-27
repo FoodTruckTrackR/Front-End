5118affa4e0c5014071c876f01a34750d6c981b2
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-27 16:11:40 -0700

    Styled the div wrapping the two forms, and each form in the Add_truck file

--- truck-tracker/src/Aaron_Components/Add_truck.js
+++ truck-tracker/src/Aaron_Components/Add_truck.js
@@ -1,6 +1,26 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const StyledDiv = styled.div`
+    display: flex;
+    border: 2px solid #51240f;
+    border-radius: 20%;
+    padding: 3%;
+    width: 80%;
+    height: 40vh;
+    margin: 3% 3% 3% 10%;
+    color: #51240f;
+    background-color: rgba(222, 100, 89, 0.8);
+`
+
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 30vh;
+    margin-right: 8%;
+`
+
 const AddTruckComp = () => {
     const [truckState, setTruckState] = useState({
         truckName: '',
@@ -60,31 +80,34 @@ const AddTruckComp = () => {
     }
 
     return(
-        <div>
-            <form onSubmit={truckSubmit}>
-            <label>
-                What's the name of the truck? 
-                <input
-                name='truckName'
-                type='text'
-                id='truckName'
-                value={truckState.truckName}
-                onChange={truckChange}
-                />
-            </label>
-            <label>
-                What cuisine do you serve? 
-                <input
-                name='cuisineType'
-                type='text'
-                id='cuisineType'
-                value={truckState.cuisineType}
-                onChange={truckChange}
-                />
-            </label>
-            <button type='submit'>Submit truck</button>
-        </form>
-            <form onSubmit={menuSubmit}>
+        <StyledDiv>
+            <StyledForm onSubmit={truckSubmit}>
+                <h5>Add a Truck</h5>
+                <label>
+                    What's the name of the truck? 
+                    <input
+                    name='truckName'
+                    type='text'
+                    id='truckName'
+                    value={truckState.truckName}
+                    onChange={truckChange}
+                    />
+                </label>
+                <label>
+                    What cuisine do you serve? 
+                    <input
+                    name='cuisineType'
+                    type='text'
+                    id='cuisineType'
+                    value={truckState.cuisineType}
+                    onChange={truckChange}
+                    />
+                </label>
+                <button type='submit'>Submit truck</button>
+            </StyledForm>
+            <hr />
+            <StyledForm onSubmit={menuSubmit}>
+                <h5>Add a menu item</h5>
                 <label>
                     Menu item 
                     <input
@@ -116,8 +139,8 @@ const AddTruckComp = () => {
                     />
                 </label>
                 <button type='submit'>Submit item</button>
-            </form>
-        </div>
+            </StyledForm>
+        </StyledDiv>
     )
 }
 
