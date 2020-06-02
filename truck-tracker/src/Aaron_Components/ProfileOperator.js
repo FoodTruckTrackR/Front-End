978aef6505abcfa2459e2f969a82927a2756c47d
Sonica1031 <Sonica1031@hotmail.com>
2020-06-02 18:09:41 -0500

    Fixed Build

--- truck-tracker/src/Aaron_Components/ProfileOperator.js
+++ truck-tracker/src/Aaron_Components/ProfileOperator.js
@@ -1,90 +1,97 @@
-import React, { useEffect, useState } from 'react';
-import axiosWithAuth from './axiosWithAuth';
-import {connect} from 'react-redux';
-import {actionLogin} from './Action';
-import Add_truck from './Add_truck';
-import CustomerSurvey from './CustomerSurvey'
+import React, { useEffect, useState } from "react";
+import axiosWithAuth from "./axiosWithAuth";
+import { connect } from "react-redux";
+import { actionLogin } from "./Action";
+import Add_truck from "./Add_truck";
+import CustomerSurvey from "./CustomerSurvey";
+import ReactDOM from "react-dom";
 
 const ProfileOperator = (props) => {
-    console.log("profile", props)
-    const [trucks, setTrucks] = useState([]);
-        useEffect(() =>{
-            axiosWithAuth()
-                .get(`https://foodtruck-trackr.herokuapp.com/operators/${props.id}/trucks`)
-                .then(res => setTrucks(res.data))
-                .catch(err => console.log(err))
+  console.log("profile", props);
+  const [trucks, setTrucks] = useState([]);
+  useEffect(() => {
+    axiosWithAuth()
+      .get(
+        `https://foodtruck-trackr.herokuapp.com/operators/${props.id}/trucks`
+      )
+      .then((res) => setTrucks(res.data))
+      .catch((err) => console.log(err));
+  }, [props.id]);
 
-        },[props.id])
+  const [newTruck, setNewTruck] = useState({
+    truckName: "",
+    imageOfTruck: "",
+    cuisineType: "",
+  });
 
-        const onDelete = (item) => {
-            axiosWithAuth()
-                .delete(`https://foodtruck-tracker.herokuapp.com/operators/${props.id}/trucks/${item.id}`)
-                .then(res => setTrucks(res.data))
-                .catch(err => console.log(err))
-        }
-        const onUpdate = (item) => {
-            const containingDiv = document.querySelector('.update');
-            containingDiv.textContent = '';
-            const truckName = document.createElement('input');
-            truckName.type = 'text';
-            truckName.id = item.id;
-            truckName.placeholder = 'Enter Truck Name';
-            const imageOfTruck = document.createElement('input');
-            imageOfTruck.type='text';
-            imageOfTruck.placeholder = 'Enter Image URL'; 
-            const cuisineType = document.createElement('input');
-            cuisineType.type='text';
-            cuisineType.placeholder = 'Enter Cuisine Type';
-            const buttonForSubmit = document.createElement('button');
-            buttonForSubmit.textContent = 'Update';
-            containingDiv.appendChild(truckName);
-            containingDiv.appendChild(imageOfTruck);
-            containingDiv.appendChild(cuisineType);
-            containingDiv.appendChild(buttonForSubmit);
+  const onDelete = (item) => {
+    axiosWithAuth()
+      .delete(
+        `https://foodtruck-tracker.herokuapp.com/operators/${props.id}/trucks/${item.id}`
+      )
+      .then((res) => setTrucks(res.data))
+      .catch((err) => console.log(err));
+  };
 
-            const newTruck ={
-                truckName: truckName.value,
-                imageOfTruck: imageOfTruck.value,
-                cuisineType: cuisineType.value
-            }
+  const onEdit = (item) => {
+    axiosWithAuth()
+      .put(
+        `https://foodtruck-tracker.herokuapp.com/operators/${props.id}/trucks/${item.id}`,
+        newTruck
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
 
-            const onEdit = () =>{
-                axiosWithAuth()
-                .put(`https://foodtruck-tracker.herokuapp.com/operators/${props.id}/trucks/${truckName.id}`, newTruck)
-                .then(res => console.log(res))
-                .catch(err => console.log(err))
-            }
+  return (
+    <div>
+      <Add_truck />
+      <div className="truck-info">
+        {trucks.map((item) => {
+          return (
+            <div className="update">
+              <div>{item.truckName}</div>
+              <div>{item.imageOfTruck}</div>
+              <div>
+                <button onClick={onDelete(item)}>Delete</button>
+                <input
+                  placeholder="Enter New Truck Name"
+                  type="text"
+                  onChange={(e) =>
+                    setNewTruck({ ...trucks, truckName: e.target.value })
+                  }
+                />
+                <input
+                  placeholder="Enter New Image URL Here"
+                  type="text"
+                  onChange={(e) =>
+                    setNewTruck({ ...trucks, imageOfTruck: e.target.value })
+                  }
+                />
+                <input
+                  placeholder="Enter New Cuisine Type Here"
+                  type="text"
+                  onChange={(e) =>
+                    setNewTruck({ ...trucks, cuisineType: e.target.value })
+                  }
+                />
+                <button onClick={onEdit(item)}>Edit</button>
+              </div>
+              <CustomerSurvey item={item} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-            buttonForSubmit.addEventListener('click', onEdit)
-        }
-    return(
-        <div>
-            <Add_truck />
-            {
-                trucks.map(item => {
-                    return(
-                    <div className="update">
-                    <div>{item.truckName}</div>
-                    <div>{item.imageOfTruck}</div>
-                    <div>
-                    <button onClick={onDelete(item)}>Delete</button>
-                    <button onClick={onUpdate}>Update</button>
-                    <CustomerSurvey item={item} />
-                    </div>
-                    </div>
-                )
-                })
-            }
-        </div>
-    )
-}
+const mapStateToProps = (state) => {
+  return {
+    error: state.error,
+    id: state.id,
+    isFetching: state.isFetching,
+  };
+};
 
-const mapStateToProps = state =>{
-    return{
-        error: state.error,
-        id: state.id,
-        isFetching: state.isFetching
-    }
-}
-
-export default connect(mapStateToProps, {actionLogin})(ProfileOperator);
+export default connect(mapStateToProps, { actionLogin })(ProfileOperator);
